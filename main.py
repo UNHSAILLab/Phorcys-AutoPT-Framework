1ee1742358a818aac3882248c86a02144b040379
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-02-03 19:59:49 -0500

    Configurations

---
+++ main.py
@@ -0,0 +1,38 @@
+#Dealing with arguments
+
+import argparse
+import ipaddress
+import socket
+
+def arguments():
+    parser = argparse.ArgumentParser(description = 'Phorcys Automated Penetration Testing Tool')
+    parser.add_argument('-i', metavar='I', type=str, help="Target in IPV4 or IPV6 format)")
+    parser.add_argument('-c', type = str, help="Target in CIDR notation")
+    parser.add_argument('-d', type = str, help="Target in Domain Name format")
+    
+    args = parser.parse_args()
+
+    if args.i:
+        IP = args.i
+        ipaddress.ip_address(IP)
+        print(IP)
+    if args.c:
+        CIDR = args.c
+        ipaddress.ip_network(CIDR)
+        print(CIDR)
+
+    if args.d:
+        DOMAIN = args.d
+        try:
+            DATA = socket.gethostbyname(DOMAIN)
+            IP_ADDRESS = repr(DATA)
+            return IP_ADDRESS
+        except Exception:
+            print("Invalid Domain Name")
+            return False
+        
+        print(IP_ADDRESS)
+
+
+if __name__ == '__main__':
+    arguments()
