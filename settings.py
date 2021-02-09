366e12bd4984c48b21af3f19e6c6578b19a3f69a
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-02-09 10:11:23 -0500

    Update of Configurations - Addition of Metasploit Password

--- settings.py
+++ settings.py
@@ -7,9 +7,8 @@ config.read('config.ini')
 print(config.sections())
 
 
-
-# config["Metasploit"] = {'ServerIP': '127.0.0.1', 'ServerPort': '55552'}
-# config["NetAttacker"] = {'IPAddress': '127.0.0.1', 'Port': '5000', 'APIKey': "XXXXXX"}
+# config["Metasploit"] = {'ServerIP': '127.0.0.1', 'ServerPort': '55552', 'Password':'XXXXXXX'}
+# config["Nettacker"] = {'IPAddress': '127.0.0.1', 'Port': '5000', 'APIKey': "XXXXXX"}
 
 # with open('config.ini', 'w') as configfile:
 #     config.write(configfile)
