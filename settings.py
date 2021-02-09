a6a6be04320465953c8f8b5b80ccf059c98f9605
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-02-09 10:05:55 -0500

    Update of settings.py to read from config.ini

--- settings.py
+++ settings.py
@@ -3,9 +3,13 @@
 import configparser
 
 config = configparser.ConfigParser()
+config.read('config.ini')
+print(config.sections())
 
-config["Metasploit"] = {'ServerIP': '127.0.0.1', 'ServerPort': '55552'}
-config["NetAttacker"] = {'IPAddress': '127.0.0.1', 'Port': '5000', 'APIKey': "XXXXXX"}
 
-with open('config.ini', 'w') as configfile:
-    config.write(configfile)
+
+# config["Metasploit"] = {'ServerIP': '127.0.0.1', 'ServerPort': '55552'}
+# config["NetAttacker"] = {'IPAddress': '127.0.0.1', 'Port': '5000', 'APIKey': "XXXXXX"}
+
+# with open('config.ini', 'w') as configfile:
+#     config.write(configfile)
