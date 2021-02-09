50e2c877382ee67970cef218a260abcc5f69d438
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-02-09 09:55:43 -0500

    First Iteration of ConfigParser

---
+++ settings.py
@@ -0,0 +1,11 @@
+#This file contains the config parsing - object
+
+import configparser
+
+config = configparser.ConfigParser()
+
+config["Metasploit"] = {'ServerIP': '127.0.0.1', 'ServerPort': '55552'}
+config["NetAttacker"] = {'IPAddress': '127.0.0.1', 'Port': '5000', 'APIKey': "XXXXXX"}
+
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
