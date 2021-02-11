cae4bca0892e5c7356510c41a6ab9f42ee5e63ff
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-02-11 14:09:30 -0500

    Update of Comments

--- modules/settings.py
+++ modules/settings.py
@@ -4,8 +4,8 @@
 
 class Singleton(type):
     """TODO: write comments.
-    The purpose of a Singleton is to provide access to a single object that is needed across an entire software platform. This ensures that if the object is changed,
-    it stays uniform for all areas of a project that need it.
+    The purpose of a Singleton is to provide access to a single object that is needed across an entire software platform
+    This ensures that if the object is changed, it stays uniform for all areas of a project that need it.
 
     Below initializes an instance of the object.
     """
@@ -17,8 +17,9 @@ class Singleton(type):
         return cls.instance
 
 class Settings(metaclass=Singleton):
-    """TODO: write comments.
-        This class creates a Singleton for all of the necessary settings for the platform. All portions of the system can access this information as needed.
+    """
+        This class creates a Singleton for all of the necessary settings for the platform. All portions of the system
+        can access this information as needed.
 
         __init__:
         :param str nettacker_ip: IP address for the nettacker API Server
@@ -50,66 +51,64 @@ class Settings(metaclass=Singleton):
         self.target = target
 
         # make sure all values are unpacked.
-        """ This ensures that all values in the dictionary are initialized """
+        """ This ensures that all values in the dictionary are initialized"""
         for key in self.__dict__:
             if self.__dict__[key] is None:
                 raise KeyError(f"Missing Parameter: {key}")
 
-
-    """ The following functions return the settings information:
-
-        get_nettacker_port():
-        :param self - This tells the program to access the nettacker_port initialized in the instance parameters
-        :return nettacker_port  - Returns the port
-
-        get_nettacker_ip():
-        :param self - This tells the program to access the nettacker_ip initialized in the instance parameters
-        :return nettacker_IP - Returns the IP address hosting nettacker
-
-        get_nettacker_key():
-        :param self - This tells the program to access the nettacker_key initialized in the instance parameters
-        :return nettacker_key - Returns the nettacker API Key initialized in the instance
-
-        get_metasploit_ip():
-        :param self - This tells the program to access the metasploit_ip initialized in the instance parameters
-        :return metasploit_ip: Returns the IP that is hosting the Metsploit RPC API server
-
-        get_metasploit_port():
-        :param self - This tells the program to access the metasploit_port initialized in the instance parameters
-        :return metasploit_port: Returns the Port to the Metsploit RPC API server
-
-        get_metasploit_password():
-        :param self - This tells the program to access the metasploit_password initialized in the instance parameters
-        :return self.metasploit_port: Returns the password to the Metsploit RPC API server
-
-        get_target():
-        :param self - This tells the program to access the target initialized in the instance parameters
-        :return self.target: Returns the Target IP Address, Domain, or range
-
-        get_dict():
-        :param self - This tells the program to access and create the dictionary of all the settings parameters that have been initialized
-        :return self.__dict__  - Returns the dictionary of all the necessary settings
-    """
     def get_nettacker_port(self) -> int:
+        """ This returns the port information for Nettacker
+            :param self - This tells the program to access the port that has been initialized in the instance
+            :return self.nettacker_port  - Returns the port
+        """
         return self.nettacker_port
-    
+        
     def get_nettacker_ip(self) -> str:
+        """ This returns the IP address of the nettacker API
+            :param self - This tells the program to access the nettacker_ip initialized in the instance parameters
+            :return nettacker_IP - Returns the IP address hosting nettacker
+        """
         return self.nettacker_ip
 
     def get_nettacker_key(self) -> str:
+        """ This returns the API Key needed for Nettacker API
+            :param self - This tells the program to access the nettacker_key initialized in the instance parameters
+            :return nettacker_key - Returns the nettacker API Key initialized in the instance
+        """
         return self.nettacker_key
 
     def get_metasploit_ip(self) -> int:
+        """ Returns the Server IP for the Metasploit RPC API
+            :param self - This tells the program to access the metasploit_ip initialized in the instance parameters
+            :return metasploit_ip: Returns the IP that is hosting the Metsploit RPC API server
+        """
         return self.metasploit_ip
 
     def get_metasploit_port(self) -> str:
+        """ This returns the port for the Metasploit RPC API
+            :param self - This tells the program to access the metasploit_port initialized in the instance parameters
+            :return metasploit_port: Returns the Port to the Metsploit RPC API server
+        """
         return self.metasploit_port
 
     def get_metasploit_password(self) -> str:
+        """ This returns the Metasploit RPC API Port
+            :param self - This tells the program to access the metasploit_password initialized in the instance parameters
+            :return self.metasploit_port: Returns the password to the Metsploit RPC API server
+        """
         return self.metasploit_password
 
     def get_target(self) -> str:
+        """ Returns the Target IP, Domain or Range
+            :param self - This tells the program to access the target initialized in the instance parameters
+            :return self.target: Returns the Target IP Address, Domain, or range
+        """
+
         return self.target
 
     def get_dict(self) -> dict:
+        """ This creates a dictionary for all the necessary settings
+        :param self - This tells the program to access and create a dictionary of all initialized settings
+        :return self.__dict__  - Returns the dictionary of all the necessary settings
+        """
         return self.__dict__
