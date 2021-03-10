dbdb075de2f387e6c6835aead635bfb47666c95a
Samuel Zurowski <szuro1@unh.newhaven.edu>
2021-03-10 12:40:26 -0500

    Setting up gym environment actions

--- modules/attack_env/environment.py
+++ modules/attack_env/environment.py
@@ -2,9 +2,18 @@
 import gym, logging
 import numpy as np
 from gym import spaces
+from collections import OrderedDict
+
+
+COST_EXPLOIT = 20.0
+COST_SCAN = 10.0
+
+REWARD_SCAN = 50.0
+REWARD_EXPLOIT = 1000.0
 
 class Environment(gym.Env):
     """ Custom Environment for Gym Interface """
+    # TODO: Convert 2d actions spaces into 1d actions spaces.
 
     def __init__(self, nettacker_json):
         super(Environment, self).__init__()   
@@ -20,25 +29,102 @@ class Environment(gym.Env):
 
         # now that we have nettacker_json 
         # create the state space as the observation
-        
-        # Define action and observation space
-        # They must be gym.spaces objects  
-        
+
+        # essentially for each space create a dict.
+        self.action_space = spaces.Dict({
+            "target": spaces.MultiBinary(5), # change whenever decided will make a class to handle this.
+            "port": spaces.Box(low=-1.444, high=1.444, shape=(2,), dtype=np.float32),
+            "action": spaces.MultiBinary(5) # will need to change.
+        })
     
-        # Example when using discrete actions:
-        self.action_space = spaces.Discrete(10)    
-        
         # Full state space for observation.
-        self.observation_space = spaces.Box(low=0, high=255, shape=
-                    (10, 20, 2), dtype=np.uint8)
-    
+        self.observation_space = spaces.MultiBinary([5,5,5])
+
+        self._construct_network(nettacker_json)
+
+
+    def _construct_network(self, nettacker_json):
+
+        self.network = OrderedDict()
+        # create stateparser
+
+        # get nettacker json and add to stateparser
+
+        address_space = None # need to add refactor
+
+        # once state space code is done can add each host.
+        for host in address_space:
+            _ports = address_space.get("ports")
+            _access_level = address_space.get("access")
+            _services = address_space.get("services")
+
+            self.network[host] = {
+                "serivces": _services, 
+                "ports": _ports, 
+                "access_level": _access_level
+                # ... more just for now until i get more
+            }
+
+
+
+    def _check_action_type_cost(self, action):
+        # will need to do a translation so (scan if auxillary and exploit if exploit in string)
+        if action['type'] == 'scan':
+            return COST_SCAN
+        elif action['type'] == 'exploit':
+            return COST_EXPLOIT
+        else:
+            raise NotImplementedError
+
+
+    def _take_action(self, action):
+        # return bool success,
+        # value if success
+        # service information
+
+        target = action['target']
+        action = action['action']
+        port = action['port']
+
+        # translate first to orginal encoding
+
+        if action['action'] == 'scan':
+            # run metasploit module
+            raise "Wait for Metasploit integration"
+        elif action['action'] == 'exploit':
+            raise "Wait for Metasploit integration"
+        else: 
+
+        return True, 10, 0.0 # return will be changed once ready
+
     def step(self, action):
         """ TODO: add step of action"""
-        raise NotImplementedError
+        target, action_type = action['target'], action['action']
+
+        action_cost = self._check_action_type_cost(action_type)
+
+        # if not access level
+        if self.current_state[target]['access_level'] == [0, 0]:
+            return self.current_state, 0 - action_cost, False, {}
+
+
+
+
+
+
+
+
+
     
+
     def reset(self):
-        """ TODO: add a reset function """
-        raise NotImplementedError
+        """
+        Reset the state of the environment and returns the initial observation.
+        Returns:
+            dict obs : the intial observation of the network environment
+        """
+        self.current_state = self.observation_space.get_initial_state()
+        return self.current_state
 
 
     def render(self):
